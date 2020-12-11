2551e089a244d85dc1e6a5f16443fc8a396dbb6c
Dimitri2020007 <72486732+Dimitri2020007@users.noreply.github.com>
2020-12-11 20:45:25 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -10,5 +10,11 @@
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
 
-# Modify default IP
+# 修改默认登录ip
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+
+# 替换https-dns-proxy.init文件,解决用LEDE源码加入passwall编译固件后DNS转发127.0.0.1#5053和12.0.0.1#5054问题
+curl -fsSL https://raw.githubusercontent.com/Lienol/openwrt-packages/19.07/net/https-dns-proxy/files/https-dns-proxy.init > feeds/packages/net/https-dns-proxy/files/https-dns-proxy.init
+
+# 增加uwsgi limit-as限制大小
+nl feeds/packages/net/uwsgi/files-luci-support/luci-webui.ini | sed -i 's/limit-as = 1000/limit-as = 1000000/g' feeds/packages/net/uwsgi/files-luci-support/luci-webui.ini
